6e01a6f2ad8473730bd4db643c0d81736cad5fb8
Paar, F. (Ferdinand) <ferdinand.paar@ru.nl>
2024-10-29 22:08:08 +0100

    Xcode setup

---
+++ DaPasstWasNicht/DaPasstWasNicht/DaPasstWasNichtApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaPasstWasNichtApp.swift
+//  DaPasstWasNicht
+//
+//  Created by Ferdinand Paar on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct DaPasstWasNichtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DaPasstWasNicht/DaPasstWasNichtTests/DaPasstWasNichtTests.swift
@@ -0,0 +1,17 @@
+//
+//  DaPasstWasNichtTests.swift
+//  DaPasstWasNichtTests
+//
+//  Created by Ferdinand Paar on 29/10/2024.
+//
+
+import Testing
+@testable import DaPasstWasNicht
+
+struct DaPasstWasNichtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
